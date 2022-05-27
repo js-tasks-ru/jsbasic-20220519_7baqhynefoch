9e180dd370273afa508f85d88bee125e961d5f0e
Evgenia Isaeva <evgenia.isaeva297@gmail.com>
2022-05-27 19:11:36 +0300

    2-module/1-task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,11 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+
+  for (let item in salaries) {
+    if (typeof salaries[item] === "number" && isFinite(salaries[item])) {
+      sum = sum + salaries[item];
+    }
+  }
+
+  return sum;
 }
