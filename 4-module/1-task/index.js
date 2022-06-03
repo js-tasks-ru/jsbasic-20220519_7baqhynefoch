a4033c6d65e94c25549fc51d51605fa94f8a672e
Evgenia Isaeva <evgenia.isaeva297@gmail.com>
2022-06-03 19:04:46 +0300

    4-module/1-task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let list = document.createElement("ul");
+
+  for (let friend of friends) {
+    let listItem = document.createElement("li");
+    listItem.innerHTML = friend.firstName + " " + friend.lastName;
+    list.append(listItem);
+  }
+
+  return list;
 }
