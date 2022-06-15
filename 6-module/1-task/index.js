ac2cd93544d13b7431fd064c28175407794a41f2
Evgenia Isaeva <evgenia.isaeva297@gmail.com>
2022-06-15 09:10:56 +0300

    6-module/1-task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,45 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.createTable();
+  }
+
+  createTable() {
+    this.elem = document.createElement("table");
+
+    let thead = document.createElement("thead");
+    thead.innerHTML = `
+      <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th></th>
+      </tr>
+    `;
+    this.elem.append(thead);
+
+    let tbody = document.createElement("tbody");
+    tbody.innerHTML = this.rows
+      .map(
+        (item) => `
+			<tr>
+				<td>${item.name}</td>
+				<td>${item.age}</td>
+				<td>${item.salary}</td>
+				<td>${item.city}</td>
+				<td><button>X</button></td>
+			</tr>`
+      )
+      .join("");
+    this.elem.append(tbody);
+
+    let buttons = this.elem.querySelectorAll("button");
+    for (let btn of buttons) {
+      btn.addEventListener("click", (event) => {
+        event.target.closest("tr").remove();
+      });
+    }
   }
 }
